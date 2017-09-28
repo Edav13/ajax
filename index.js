12c51e1da57107270c56adba8ae374b05a1751c7
Edav13 <Edav13@hotmail.com>
2017-09-28 09:30:19 -0400

    ajax0002

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-function ejecutarAJAX(){
-	//var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+function mostrarDatos() {
+	var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 	//0 peticion no ha sido inicializada
 	//1 peticion no ha sido establecida
 	//2 peticion ha sido enviada
 	//3 peticion esta siendo procesada
 	//4 peticion ha sido finalizada
-	var ajaxRequest = new XMLHttpRequest();
-	console.log(ajaxRequest)
+	var resultado = document.getElementById('info');
+	//var ajaxRequest = new XMLHttpRequest();
 	ajaxRequest.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("info").innerHTML = this.responseText;
+		if (this.readyState === 4 && this.status === 200) {
+			var datos = JSON.parse(this.responseText);
+			resultado.innerHTML = '';
+			for (var i in datos) {
+				resultado.innerHTML += i + ': ' +datos[i]+'<br>';
+			}
 		}
 	};
-	ajaxRequest.open("GET", "archivo.txt", true);
+	ajaxRequest.open("GET", "datos.json", true);
 	ajaxRequest.send();
 }
