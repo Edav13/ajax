e63b66264b509326a9fe4f1602276f46b0ef9c5d
Edav13 <Edav13@hotmail.com>
2017-09-29 14:11:50 -0400

    ajax0006

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-function mostrarDatosJSON() {
+function mostrarDatosJSON(nombre) {
     var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     var resultado = document.getElementById('info');
-    ajaxRequest.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            var datos = JSON.parse(this.responseText);
-            resultado.innerHTML = '';
-            var min = [];
-            //var min = datos[0].Edad
-            datos.forEach(function(datoSimple){
-                min.push(datoSimple.Edad);
-                //min = min < datoSimple.Edad ? min : datoSimple.Edad;
-            });
-            resultado.innerHTML += 'La edad menor es:' + Math.min.apply(null, min);
-        }
-    };
-    ajaxRequest.open("GET", "datos.json", true);
-    ajaxRequest.send();
+    console.log(nombre.lenght)
+    if(nombre === '' || nombre === undefined) {
+        resultado.innerHTML = '';
+    }else {
+        ajaxRequest.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                var datos = JSON.parse(this.responseText);
+                var min = [];
+                datos.forEach(function(datoSimple){
+                    min.push(datoSimple.Nombre);
+                });
+                resultado.innerHTML = min.indexOf(nombre) > -1 ? '<br>Nombre encontrado': '<br>Nombre no encontrado'
+            }
+        };
+        ajaxRequest.open("GET", "datos.json", true);
+        ajaxRequest.send();
+    }
 }
 //0 peticion no ha sido inicializada
 //1 peticion no ha sido establecida
