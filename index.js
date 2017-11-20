6633f58103eb47b5a51efcb56da0ae2a0e20fb13
Edav13 <Edav13@hotmail.com>
2017-11-20 11:07:28 -0400

    ajax0019 - ajax 0025

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var resultado = document.getElementById('mostrar');
+var resultado = document.getElementById('info');
 
-function mostrarClientes() {
+function mostrarUsuarios() {
     var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 
     ajaxRequest.onreadystatechange = function() {
@@ -8,40 +8,89 @@ function mostrarClientes() {
             resultado.innerHTML  = ajaxRequest.responseText;
         }
     };
-    ajaxRequest.open('GET', 'servidor.php', true);
+    ajaxRequest.open('GET', 'servidor.php?personas='+'personas', true);
     ajaxRequest.send();
 
 }
-function buscarUsuario(nombre) {
-    var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+mostrarUsuarios();
 
-    ajaxRequest.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            resultado.innerHTML  = ajaxRequest.responseText;
-        }
-    };
-    ajaxRequest.open('GET', 'servidor.php?nombre='+nombre, true);
-    ajaxRequest.send();
-}
+function editarUsuario(usuarioID) {
+    var nombreID = "nombreID" + usuarioID;
+    var emailID = 'emailID'+ usuarioID;
+    var borrar = 'borrar' + usuarioID;
+    var actualizar = 'actualizar' + usuarioID;
+    var editarNombreID = nombreID + '-editar';
 
-function toggleOverlay(elemento) {
-    var overlay = document.getElementById('overlay');
-    var informacionDelUsuario = document.getElementById('informacionDelUsuario');
-    var info = document.getElementById('info');
-    overlay.style.opacity = 0.6;
-    console.log(overlay.style.display)
-    if(overlay.style.display === "block") {
-        overlay.style.display = "none";
-        informacionDelUsuario.style.display = "none";
-    } else {
-        overlay.style.display = "block";
-        informacionDelUsuario.style.display = "block";
+    var nombreDelUsuario = document.getElementById(nombreID).innerHTML;
+
+    var parent = document.querySelector('#' + nombreID);
+    if(parent.querySelector('#' + editarNombreID) === null) {
+        document.getElementById(nombreID).innerHTML = '<input type="text" id="'+editarNombreID+'" value="'+nombreDelUsuario+'">';
+        document.getElementById(borrar).disabled = true;
+        document.getElementById(actualizar).style.display = 'block';
     }
-    var nombre = elemento.innerHTML;
-    var correo = elemento.nextSibling.value;
-    info.innerHTML = '<strong>Nombre: </strong>' + nombre +'<br>'+
-                    '<strong>Correo: </strong>' + correo +'<br>';
-}   
+}
+
+function actualizarUsuario(usuarioID) {
+    var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    var nombreActualizado = document.getElementById('nombreID' + usuarioID + '-editar').value;
+    ajaxRequest.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            mostrarUsuarios();
+        }
+    };
+
+    ajaxRequest.open('GET', 'servidor.php?usuarioIDActualizado=' + usuarioID + '&nombreActualizado=' + nombreActualizado, true);
+    ajaxRequest.send();
+}
+
+function borrarUsuario(usuarioID) {
+    var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    var respuesta = confirm('Estas seguro de borrar este usuario?');
+    if(respuesta) {
+        ajaxRequest.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                mostrarUsuarios();
+            }
+        };
+
+        ajaxRequest.open('GET', 'servidor.php?usuarioIDEliminado=' + usuarioID, true);
+        ajaxRequest.send();
+    }
+}
+
+var overlay = document.getElementById('overlay');
+var nuevaVentana = document.getElementById('nuevaVentana');
+
+function ejecutarNuevaVentana(){
+    overlay.style.opacity = 0.5;
+    if(overlay.style.display === 'block') {
+        overlay.style.display = 'none';
+        nuevaVentana.style.display = 'none';
+    } else {
+        overlay.style.display = 'block';
+        nuevaVentana.style.display = 'block';
+    }
+    document.getElementById('nuevoUsuarioID').value = '';
+    document.getElementById('nuevoEmailID').value = '';
+}
+
+function agregarUsuario() {
+
+    overlay.style.display = 'none';
+    nuevaVentana.style.display = 'none';
+    var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+    var nuevoUsuario = document.getElementById('nuevoUsuarioID').value;
+    var nuevoEmail = document.getElementById('nuevoEmailID').value;
+    ajaxRequest.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            mostrarUsuarios();
+        }
+    };
+    ajaxRequest.open('GET', 'servidor.php?nuevoUsuario=' + nuevoUsuario + '&nuevoEmail=' + nuevoEmail, true);
+    ajaxRequest.send();
+}
 //0 peticion no ha sido inicializada
 //1 peticion no ha sido establecida
 //2 peticion ha sido enviada
--- servidor.php
+++ servidor.php
@@ -1,27 +1,57 @@
 <?php
     require "conexion.php";
-    if(!empty($_GET['nombre'])){
-        $nombre = $_GET['nombre'];
-    }
-    if(!empty($nombre)) {
-        $cliente = mysqli_real_escape_string($con, $nombre);
-        $resultadoBD = mysqli_query($con, "SELECT * FROM personas WHERE nombre LIKE '%".$cliente."%'");
-        while($fila = mysqli_fetch_assoc($resultadoBD)) {
-            echo '<div class="miClase" onclick="toggleOverlay(this)" >'.$fila['nombre'].'</div>';
-            echo '<input type="hidden" value="'.$fila['correo'].'"/>';
-        }
-        mysqli_close($con);
-    } else {
-        mostrarUsuarios();
-    }
-    function mostrarUsuarios() {
-        require "conexion.php";
-        $resultado = mysqli_query($con, "SELECT * FROM personas");
 
-        while($fila = mysqli_fetch_assoc($resultado)) {
-            echo '<div class="miClase" onclick="toggleOverlay(this)">'.$fila['nombre'].'</div>';
-            echo '<input type="hidden" value="'.$fila['correo'].'"/>';
+    $personas = $_GET['personas'];
+
+    $nombreID = "nombreID";
+    $emailID = "emailID";
+    $actualizar = "actualizar";
+    $borrar = "borrar";
+
+    if($personas === "personas") {
+        $resultadoBD = mysqli_query($con, "SELECT * FROM personas");
+
+        $table = '<div class="container">';
+        $table .= '<table class="table table-striped table-bordered">';
+        $table .= '<tr>';
+        $table .= '<th>Usuario</th>';
+        $table .= '<th>Nombre</th>';
+        $table .= '<th>Email</th>';
+        $table .= '<th>Editar Usuario</th>';
+        $table .= '<th>Borrar Usuario</th>';
+        $table .= '</tr>';
+        while($fila = mysqli_fetch_assoc($resultadoBD)) {
+            $table .= '<tr>';
+            $table .= '<td>' . $fila['usuario'] . '</td>';
+            $table .= '<td id="'.$nombreID.$fila['usuario'].'">' . $fila['nombre'] . '</td>';
+            $table .= '<td id="'.$emailID.$fila['usuario'].'">' . $fila['correo'] . '</td>';
+            $table .= '<td><input id="'.$fila['usuario'].'" onclick="editarUsuario(this.id)" type="button" value="Editar" class="btn btn-default"></td>';
+            $table .= '<td><input id="'.$borrar.$fila['usuario'].'" onclick="borrarUsuario('.$fila['usuario'].')" type="button" value="Borrar" class="btn btn-danger"></td>';
+            $table .= '<td><input id="'.$actualizar.$fila['usuario'].'" onclick="actualizarUsuario('.$fila['usuario'].')" type="button" value="Actualizar" class="btn btn-primary" style="display:none"></td>';
+            $table .= '</tr>';
         }
+        $table .= '</table>';
+        $table .= '<button onclick="ejecutarNuevaVentana()" class="btn btn-primary">Agregar Usuario</button>';
+
+        echo $table;
+        mysqli_close($con);
+    }
+    if(!empty($_GET['nombreActualizado'])) {
+        $usuarioIDActualizado = $_GET['usuarioIDActualizado'];
+        $nombreActualizado = $_GET['nombreActualizado'];
+        $cliente = mysqli_real_escape_string($con, $nombreActualizado);
+        $resultado = mysqli_query($con, "UPDATE personas SET nombre = '$cliente' WHERE usuario = $usuarioIDActualizado");
+        mysqli_close($con);
+    }
+    if(!empty($_GET['usuarioIDEliminado'])) {
+        $usuarioIDEliminado = $_GET['usuarioIDEliminado'];
+        $resultado = mysqli_query($con, "DELETE FROM personas WHERE usuario = $usuarioIDEliminado");
+        mysqli_close($con);
+    }
+    if(!empty($_GET['nuevoUsuario']) && !empty($_GET['nuevoEmail'])) {
+        $nuevoUsuario = $_GET['nuevoUsuario'];
+        $nuevoEmail = $_GET['nuevoEmail'];
+        $resultado = mysqli_query($con, "INSERT into personas values('', '$nuevoUsuario', '$nuevoEmail')");
         mysqli_close($con);
     }
 ?>
