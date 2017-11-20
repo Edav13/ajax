7db49920b14882b62c20b56cb2330aebb629dd2f
Edav13 <Edav13@hotmail.com>
2017-11-20 11:24:46 -0400

    ajax0026

--- index.js
+++ index.js
@@ -1,96 +1,22 @@
-var resultado = document.getElementById('info');
+function ejecutarAjax() {
+    var resultado = document.getElementById('info');
+    var arr = [];
+    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 
-function mostrarUsuarios() {
-    var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-
-    ajaxRequest.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            resultado.innerHTML  = ajaxRequest.responseText;
+            arr.push(xmlhttp.responseXML.getElementsByTagName('nombre').item(0));
+            arr.push(xmlhttp.responseXML.getElementsByTagName('apellido').item(0));
+            arr.push(xmlhttp.responseXML.getElementsByTagName('edad').item(0));
+
+            resultado.innerHTML = arr[0].firstChild.nodeValue + " " + arr[1].firstChild.nodeValue + " tiene " + arr[2].firstChild.nodeValue + " años";
         }
     };
-    ajaxRequest.open('GET', 'servidor.php?personas='+'personas', true);
-    ajaxRequest.send();
+    xmlhttp.open('GET', 'datos.xml', true);
+    xmlhttp.send();
 
 }
-mostrarUsuarios();
 
-function editarUsuario(usuarioID) {
-    var nombreID = "nombreID" + usuarioID;
-    var emailID = 'emailID'+ usuarioID;
-    var borrar = 'borrar' + usuarioID;
-    var actualizar = 'actualizar' + usuarioID;
-    var editarNombreID = nombreID + '-editar';
-
-    var nombreDelUsuario = document.getElementById(nombreID).innerHTML;
-
-    var parent = document.querySelector('#' + nombreID);
-    if(parent.querySelector('#' + editarNombreID) === null) {
-        document.getElementById(nombreID).innerHTML = '<input type="text" id="'+editarNombreID+'" value="'+nombreDelUsuario+'">';
-        document.getElementById(borrar).disabled = true;
-        document.getElementById(actualizar).style.display = 'block';
-    }
-}
-
-function actualizarUsuario(usuarioID) {
-    var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    var nombreActualizado = document.getElementById('nombreID' + usuarioID + '-editar').value;
-    ajaxRequest.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            mostrarUsuarios();
-        }
-    };
-
-    ajaxRequest.open('GET', 'servidor.php?usuarioIDActualizado=' + usuarioID + '&nombreActualizado=' + nombreActualizado, true);
-    ajaxRequest.send();
-}
-
-function borrarUsuario(usuarioID) {
-    var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    var respuesta = confirm('Estas seguro de borrar este usuario?');
-    if(respuesta) {
-        ajaxRequest.onreadystatechange = function() {
-            if (this.readyState === 4 && this.status === 200) {
-                mostrarUsuarios();
-            }
-        };
-
-        ajaxRequest.open('GET', 'servidor.php?usuarioIDEliminado=' + usuarioID, true);
-        ajaxRequest.send();
-    }
-}
-
-var overlay = document.getElementById('overlay');
-var nuevaVentana = document.getElementById('nuevaVentana');
-
-function ejecutarNuevaVentana(){
-    overlay.style.opacity = 0.5;
-    if(overlay.style.display === 'block') {
-        overlay.style.display = 'none';
-        nuevaVentana.style.display = 'none';
-    } else {
-        overlay.style.display = 'block';
-        nuevaVentana.style.display = 'block';
-    }
-    document.getElementById('nuevoUsuarioID').value = '';
-    document.getElementById('nuevoEmailID').value = '';
-}
-
-function agregarUsuario() {
-
-    overlay.style.display = 'none';
-    nuevaVentana.style.display = 'none';
-    var ajaxRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-
-    var nuevoUsuario = document.getElementById('nuevoUsuarioID').value;
-    var nuevoEmail = document.getElementById('nuevoEmailID').value;
-    ajaxRequest.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            mostrarUsuarios();
-        }
-    };
-    ajaxRequest.open('GET', 'servidor.php?nuevoUsuario=' + nuevoUsuario + '&nuevoEmail=' + nuevoEmail, true);
-    ajaxRequest.send();
-}
 //0 peticion no ha sido inicializada
 //1 peticion no ha sido establecida
 //2 peticion ha sido enviada
