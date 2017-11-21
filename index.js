8062129ae792012647cefe26a5380619eab396cd
Edav13 <edav13@hotmail.com>
2017-11-20 21:20:36 -0400

    ajax0027

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function ejecutarAjax() {
     var resultado = document.getElementById('info');
-    var arr = [];
     var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            arr.push(xmlhttp.responseXML.getElementsByTagName('nombre').item(0));
-            arr.push(xmlhttp.responseXML.getElementsByTagName('apellido').item(0));
-            arr.push(xmlhttp.responseXML.getElementsByTagName('edad').item(0));
-
-            resultado.innerHTML = arr[0].firstChild.nodeValue + " " + arr[1].firstChild.nodeValue + " tiene " + arr[2].firstChild.nodeValue + " años";
+            if(this.responseXML !== null) {
+                var xmlDoc = xmlhttp.responseXML;
+                var tabla = '<tr><th>Nombre</th><th>Apellido</th></tr>';
+                var persona = xmlDoc.getElementsByTagName('cliente');
+                for(var i=0; i < persona.length;i++){
+                    tabla += '<tr><td>' + persona[i].getElementsByTagName('nombre')[0].childNodes[0].nodeValue + '</td><td>' + persona[i].getElementsByTagName('apellido')[0].childNodes[0].nodeValue + '</td></tr>'
+                }
+                resultado.innerHTML = tabla;
+            }
         }
     };
     xmlhttp.open('GET', 'datos.xml', true);
